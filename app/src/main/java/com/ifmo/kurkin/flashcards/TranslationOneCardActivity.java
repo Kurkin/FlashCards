2acff76a5fe0fb5165dc6f7882751c28d2c906d1
Dmitry Kurkin <kurkindmit@gmail.com>
2015-12-28 20:19:25 +0300

    layout fix

--- app/src/main/java/com/ifmo/kurkin/flashcards/TranslationOneCardActivity.java
+++ app/src/main/java/com/ifmo/kurkin/flashcards/TranslationOneCardActivity.java
@@ -3,12 +3,12 @@ package com.ifmo.kurkin.flashcards;
 public class TranslationOneCardActivity extends OneCardActivity {
     @Override
     void onBeginState(Card card) {
-        getWord().setText(card.lang1);
+        getWord().setText(card.lang2);
         getTranslation().setText("");
     }
 
     @Override
     void onEndState(Card card) {
-        getTranslation().setText(card.lang2);
+        getTranslation().setText(card.lang1);
     }
 }
--- app/src/main/java/com/ifmo/kurkin/flashcards/WordOneCardActivity.java
+++ app/src/main/java/com/ifmo/kurkin/flashcards/WordOneCardActivity.java
@@ -3,12 +3,12 @@ package com.ifmo.kurkin.flashcards;
 public class WordOneCardActivity extends OneCardActivity {
     @Override
     void onBeginState(Card card) {
-        getWord().setText(card.lang2);
+        getWord().setText(card.lang1);
         getTranslation().setText("");
     }
 
     @Override
     void onEndState(Card card) {
-        getTranslation().setText(card.lang1);
+        getTranslation().setText(card.lang2);
     }
 }
